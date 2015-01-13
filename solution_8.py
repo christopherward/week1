84adc61e4ba351e9f3104cf9e5f6be0f2391cd15
christopherward <christopher.ward5@gmail.com>
2015-01-12 19:54:11 -0600

    1.12.15 Update
    Updated solution_8.py to handle exceptions

--- solution_8.py
+++ solution_8.py
@@ -2,13 +2,20 @@
 #Christopher Ward
 #MPCS 50101, Winter 2015
 #This program converts a Fahrenheit temperature to Celsius
-#and reports the temperature as a whole number
+#and reports the temperature as a rounded whole number
 def FtoC():
-    fahrenheit = eval(input("What is the Fahrenheit temperature? "))
-    celsius = int(round(((fahrenheit - 32) * 5/9),0))
-    if celsius == 1:
-        print("The temperature is", celsius, "degree Celsius.")
-    else:
-        print("The temperature is", celsius, "degrees Celsius.")
+    try:
+        fahrenheit = eval(input("What is the Fahrenheit temperature? "))
+        celsius = int(round(((fahrenheit - 32) * 5/9),0))
+        if celsius == 1 or celsius == -1:
+            print("\nThe temperature is", celsius, "degree Celsius.")
+        else:
+            print("\nThe temperature is", celsius, "degrees Celsius.")
 
+    except NameError:
+        print("\nYou didn't enter a number.")
+    except SyntaxError:
+        print("\nYour input was not in the correct form.")
+    except:
+        print("\nSomething went terribly, terribly wrong.")
 FtoC()
